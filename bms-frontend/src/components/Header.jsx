9e6fbee95ee86b9d9b2194f0ba36d223f7b4dcc7
Austin <austin.150807@gmail.com>
2026-01-18 23:48:29 +0530

    first commit

---
+++ bms-frontend/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="w-full text-sm bg-white">
+      {/* Top Navbar */}
+      <div></div>
+    </div>
+  );
+};
+
+export default Header;
